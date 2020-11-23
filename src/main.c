eeaf9b5ee1210b5d48ba51cf61409ef50923f675
キーボードスレイヤー <dalcjor@libmail.eu>
2020-11-23 14:23:22 +0100

    Improved makefile, added main and unused macro

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <utils/macro.h>
+
+int 
+main(int argc, char* argv[])
+{
+	__unused(argc);
+	__unused(argv);
+
+	return 0;
+}
---
+++ src/utils/macro.h
@@ -0,0 +1,6 @@
+#ifndef _IDS_UTILS_MACRO_H_
+#define _IDS_UTILS_MACRO_H_
+
+#define __unused(x) ((void) x)
+
+#endif /* !_IDS_UTILS_MACRO_H_ */
